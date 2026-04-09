767e232b8177149b4d85cf8db919262f513af2f5
Théa <94928249+TheaCOLINOT@users.noreply.github.com>
2026-04-09 22:53:10 +0200

    add watchlist view and stores

--- src/views/WatchList.vue
+++ src/views/WatchList.vue
@@ -1,7 +1,19 @@
-<script setup></script>
+<script setup>
+import { useWatchlistStore } from '@/stores/watchlist'
+const store = useWatchlistStore()
+
+const testMovie = { imdbID: 'tt0111161', Title: 'The Shawshank Redemption' }
+
+const ajouter = () => store.ajouter(testMovie)
+const retirer = () => store.retirer(testMovie.imdbID)
+const marquerVu = () => store.toggleVu(testMovie.imdbID)
+</script>
 
 <template>
-  <h1>Tu es dans la watch list !</h1>
+  <div>
+    <button @click="ajouter">Ajouter film</button>
+    <button @click="marquerVu">Marquer vu</button>
+    <button @click="retirer">Retirer</button>
+    <pre>{{ store.items }}</pre>
+  </div>
 </template>
-
-<style scoped></style>
