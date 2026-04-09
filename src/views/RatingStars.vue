003910fd6990626f9a1b8adde53489fe37d65956
Théa <94928249+TheaCOLINOT@users.noreply.github.com>
2026-04-09 23:03:49 +0200

    create RatingStars.vue

---
+++ src/views/RatingStars.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+const note = defineModel()
+const props = defineProps({ readonly: Boolean })
+const survol = ref(0)
+</script>
+<template>
+  <div>
+    <span
+      v-for="i in 5"
+      :key="i"
+      @click="!props.readonly && (note = i)"
+      @mouseenter="!props.readonly && (survol = i)"
+      @mouseleave="survol = 0"
+      >{{ i <= (survol || note) ? '★' : '■' }}</span
+    >
+  </div>
+</template>
